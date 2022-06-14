6f6a0bd6a42873cf78d0a0f1d3adc19bc54e2b2f
Javier Perez <javierosp97@gmail.com>
2022-06-14 07:21:56 +0100

    Changed build image

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,9 @@ FROM centos:7
 WORKDIR /root
 RUN yum install nano git wget epel-release zip unzip -y
 RUN mkdir -p /etc/nginx/conf.d 
-RUN git clone https://gist.github.com/d0a8ddcfb5fc8d058252f6431adbb633.git
-RUN cd d0a8ddcfb5fc8d058252f6431adbb633 && chmod +x nginx.sh  && ./nginx.sh
+#RUN git clone https://gist.github.com/d0a8ddcfb5fc8d058252f6431adbb633.git
+COPY Files/compile.sh /root/
+RUN cd /root && chmod +x compile.sh  && ./compile.sh
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 RUN chmod +x /docker-entrypoint.sh
 COPY nginx.conf /usr/local/nginx/conf/nginx.conf
---
+++ Files/compile.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+echo " PLEASE READ THE END OF THE SCRIPT, IT IS IMPORTANT"
+cat << EOF
+*,************,(&&&&&&&&@&&&&%,,,,,,,,,*,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+***,,***,,,,,**&&&@&&&&&&&&&&&&,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,*
+**,,,,*,,**,,*%&&&@&&&&&&&&&&&&@,,,*,,*,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+,**,,,,,,,,,,,&&&&&&&&&&&&&&&&&&&,,,,,,,,,*,,,,,,,,,,,,,*,,,,,,.,.,,,,,,,,,,,*%%
+,,,,,,,,,,,,,*&&&&&&&@&&@@&@&@@&&&,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.,,,,*&&&&&&
+,,,,,,,,,,,,,,%&&&@&&&@&@@@@&@@&&&&%%%%%%%%%%((*,,,,,,,,,,,,,,,.,,,,*(%&&&&&&&&&
+,,,,,,,,,,,,,,&&&@@&@&@&&@@@@@@&@@&&&&&&&&&&&&&&%%%%(///*****(%&%&&&&&&&&&&&&&&/
+,,,,,,,,,,,,,*&&&&&@@&@@@@@@@@@@@@@@&&&&&&&&&&&&&&&&&&&%&&&&&&&&&&&&&&&&&%%&%/,,
+,,,,,,,,,,,,,*&&&&@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%&%&/,,,,
+,,,,,,,,,,,,,,#&&&@@@@@@&&@@@@@@@@@@@@@@&&&&&&@@@&@&&&&&@&&&&@&&&&&&&&&&&%*,,,,,
+,,,,,,,,,,,,,,#&@@&&@@@@&@@@@@@&@@@@@@@@@@@@@@@@@&@&&&&&&&&@@&&&&&&&&&&&*,,,...,
+,,,,,,,,,,,,(&&&&@&&@&@@@@@@@@@@@@@@@@@@@@@@@@@@@&@@&@@@@@@&@@&&&&&&&&&*....,...
+,,,,,,,,,,*#&&&&&&@@@@@@@@@@@@@@&@&@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&%/.,,,,,,..
+,,,,,,,,,*#%&&&&&@@@&&@@&&&&&&&&@&@@@@@@@@@@@@@@@@@@@@@@@@@@&@&&&&&&&,,.,.,,..,.
+,,,,,,,,,(%&&&&&&&&&&&&&@&&&&@&&&&@@@@@@@@@@@@@@@@@@@@@@&@&@&&@&&&&&%/,,.....,..
+,,,,,,,,*#&&&&&&&&&@&@&@@@@@&&&&&&&&@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&%%*..,,..,..
+,,,,,,,,*#&&&&&&&&&@&@@@@@&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&%%/,...,....
+,,,,,,,,/%&&&&&@&&@@@@@@@@@@&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@&&&&%&&&&%%%(,,..,....
+,,,,,,,*/&&&&&&&&&&&&@@@@@&@&%&@@@@@@@@@@@@@@@@@@@@@@@&@@&&&&&&&%%&&%%/,.....,,.
+,,,,,,,*(&&&&&&&&@&&&@@@&&&%%%&@@@@&&@@@@@@@@@@@@@@@@@&@@&&&%%&&%%%&%#,.........
+,,,,,,,*/%&&&&&&&&&&@@@@@@@&&&/(%%&&@@@@@@@@@@@@@@@@@@&%&&&@&#&&&&%%%*...,......
+,,,,,,,*#&&&&&&&@@@@@&&@@@@@@@&&&&&&%@&@@@@@@@@@@@@@&@#/#%&#(#%&&%%%/,,,........
+,,,,,,,/%&&&&@@@@@@@@@&@@@@@@@@@@@&@&&&@@@@@@@@@@@@@%&&%%%%%&&&&%%#/,...........
+,,,,,,,#&&&&&@@@@@@@@@@@@@@@@@@@@&&&&&@@@@@@&&&@@@@&&&&&&&&&&&%%%#*,............
+,,,,,,/%&&&&&&@@@@@@@@@@@@@@@@@@&&&&&&@@@@@&&&&&@@@&&&&&&&&&&%%%(,.....,.....,..
+,,,,,*#&&&&&@@@@@&&&&@@&&@@@@@@@&&&&&&@@@&&&&&&&&@&&&&&&&&&&&&%*,,.,.,....,.....
+,,,,*(&&&&&&@@@@@@@@@@@@&@@&&&@@@@&&&&&@@&&&&&&&&@&&&&&&&&&&&%(,....,...,.......
+,,,*/%&&&&&&&&&&@@@@@&@@@@@@@@@@@@&@@@&@@@&&&&&&&&&&&&&&&&&&&%(,,,,............,
+**/#&&&&&&&&&&@@&&@@@@@@@@&@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&%#,,,,,.......,..,.
+*/%&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@&&@@@@&@@&&&&&&&&&&&&&%#*,,,.,,.,,.....,,
+(%&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&%%*,,,,.,,...,,,,,,
+#%&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&%%%*,,.,,.,,,,.,.,,,
+%&&&@@&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&@&&&&%%%/,,,,,,,,,,,,.,,,
+&&&@@@&&&&&&&@@@@@@@@@@@@@@@@@@@%@@@@@%&%&&&&&@@@@@@@@&&&&&&&%#*,,,,,,,,,,,,,,,,
+&&&@@@&&@&&&&&@@@@@@@@@@@@@@@@&@@@@@@%%&&&%@@@@@@@@@@@@&&&&&&%/,,,,,,,,,,,,,,,,,
+&&&@@@&&@&@&&&&@@@@@@@@@@@@@@@@@@@@&&@&&&%&&&&&&@@@@&&&&&&&&%/,,,,,,,,,,,,,,,,,,
+&&@@@@&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@&@@@&&&&&@&&&&&&&&&&&&&%/*,,,,,,,,,,,,,,,,,
+&&&@@@@&&@&&&@&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&@@&&&&&&%#*,,,,,,,,,,,,,,,,,,
+&&@@@@&&&@&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&@@&&&&@&&&&&%%/**,,,,,,,,,,,.,,,,,
+@@&@@@@@&@@&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&@&@@&&@&&&&&&&%#*,,,,,,,,,,,*,,,,,,
+&&&@@@@@&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@&&@@@&@@@&&@&&&&&&&&&&%#/,,,,,,,,,,,,,,,,,
+&@@@@@@@@@@&@@@@@@@@@@@@@@@@@@@@@@@@@@@&&@@@@@&&@@@&&&&&&&&&&%#*,,,,,,,,,,,,,,,,
+@&@@@@@@&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&@@@&@&@&&&&&&&&&&&&#/,,,,,,,*,*,,,,**
+&&&@@@@@@@&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&%/*,,,,,,,,,**,,,,
+&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&%*,,,,,*,,*,*,****
+&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&@@@@@@&@@@&&&&&&&&&&&#*.,**,,,,******,*
+&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&%#*..,*************
+&@&&@&@@@@@@&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&%(*,...************
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&@&&&&&&&&&&&&&&&#*,......,**********
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&@&&&@&@&@&&&&&&&%/,........,******/**
+&@@@@@@@@@@@@@@@@&@&@@@@&&&&@@@@@@@@&@&&&&&&&&&&&&&&&&&&&##/,..........,***/*/*/
+&@@@@@@@@@@@@@@&@&@&&&&@&&&&&@&@@@@@@&&@@&&&&&&&&&&&&&&&%(*,,............*/**///
+&&&&@@@@@@@@@@@@@@&@&&@@@@&&&&&@@@@@@@&&&&&&&&&&&&&&&&&&&#*,............../#(///
+&&&&&&&@@@@@@@@@@@@@&@@@&@&&&&@@@@@@&@&&&&&&&&&&&&&&&&&&&&#,......,,,,,....*%%%%
+&&&&&@&&@@@@@@@@@@@@&&@@@&&&&&&@@@@&@&&&&&&&&&&&&&&&&&&&&&&(///////*****....,%%%
+#%%&&@@@@@&@@&@@@@@@@&&&&&@&&&&@@@&&&&&&@@@&&@@@@&&@@@&&&&&#/////////////,...,#%
+##%%&&&@@@@@@@@@@@@@@@&&&&&&&&&@@&&&&&@@&@@&@@@@@@@@@&@@@&&&(/////////////,...,#
+EOF
+sleep 5
+echo "Updating"
+yum update 
+echo "Installing the requirements for SSL"
+wget --no-check-certificate https://ftp.openssl.org/source/old/1.1.1/openssl-1.1.1.tar.gz
+yum install -y make gcc perl pcre-devel zlib-devel 
+tar xvf openssl-1.1.1.tar.gz
+echo "Changing to openssl folder"
+cd openssl-1.1.1
+./config --prefix=/usr --openssldir=/etc/ssl --libdir=lib no-shared zlib-dynamic
+echo "Compiling"
+make && make install
+echo "Exporting libraries"
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64
+echo "export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64" >> ~/.bashrc
+echo "Checking OpenSSL version"
+openssl version
+
+echo "Install dependent tools for Nginx and Modsecurity"
+yum install -y git wget epel-release gcc-c++ flex bison yajl yajl-devel curl-devel curl GeoIP-devel doxygen zlib-devel pcre-devel lmdb-devel libxml2-devel ssdeep-devel lua-devel libtool autoconf automake
+cd /usr/local
+echo "installing ModSecurity"
+git clone https://github.com/SpiderLabs/ModSecurity
+cd ModSecurity
+git checkout -b v3/master origin/v3/master
+git submodule init
+git submodule update
+sh build.sh
+./configure
+
+make && make install
+echo "Installing nginx with ModSecurity and SSL"
+cd /usr/local
+git clone https://github.com/SpiderLabs/ModSecurity-nginx
+wget http://nginx.org/download/nginx-1.22.0.tar.gz
+tar -xvzf nginx-1.22.0.tar.gz
+cd /usr/local/nginx-1.22.0
+./configure --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --with-http_ssl_module --add-module=/usr/local/ModSecurity-nginx
+make && make install
+echo "Starting nginx"
+/usr/local/nginx/sbin/nginx
+
+## WAF RULES
+echo "Waf Rules"
+mkdir -p /usr/local/nginx/conf/modsecurity
+cp /usr/local/ModSecurity/modsecurity.conf-recommended /usr/local/nginx/conf/modsecurity/modsecurity.conf
+cp /usr/local/ModSecurity/unicode.mapping /usr/local/nginx/conf/modsecurity/
+cd /usr/local
+wget http://www.modsecurity.cn/download/corerule/owasp-modsecurity-crs-3.3-dev.zip
+unzip owasp-modsecurity-crs-3.3-dev.zip
+cd owasp-modsecurity-crs-3.3-dev
+cp crs-setup.conf.example /usr/local/nginx/conf/modsecurity/crs-setup.conf
+cp -r rules /usr/local/nginx/conf/modsecurity
+cd /usr/local/nginx/conf/modsecurity/rules
+mv REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf.example REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf
+mv  RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf.example  RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf
+echo "## FOR MAKING THIS WORK YOU NEED TO CHANGE SOME THINGS
+# ON NGINX CONF --> ADD THIS ON HTTP BLOCK
+#modsecurity on;
+#modsecurity_rules_file /usr/local/nginx/conf/modsecurity/modsecurity.conf;
+# EDIT /usr/local/nginx/conf/modsecurity/modsecurity.conf 
+# CHANGE --> SecRuleEngine DetectionOnly to SecRuleEngine On
+# ADD --> Include /usr/local/nginx/conf/modsecurity/crs-setup.conf
+#         Include /usr/local/nginx/conf/modsecurity/rules/*.conf"
+
+/usr/local/nginx/sbin/nginx -s reload
