5408147a9e171f90d79caea696c6f414a7d9f7ff
Javier Perez <35831933+jaweewo@users.noreply.github.com>
2025-06-30 09:50:23 +0100

    Update compile.sh

--- Files/compile.sh
+++ Files/compile.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo " PLEASE READ THE END OF THE SCRIPT, IT IS IMPORTANT"
 cat << EOF
 *,************,(&&&&&&&&@&&&&%,,,,,,,,,*,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
@@ -106,22 +106,20 @@ cd ..
 rm -rf ModSecurity
 
 echo "Downloading and preparing Nginx with ModSecurity and NTLM..."
-wget http://nginx.org/download/nginx-1.28.0.tar.gz
-tar xvf nginx-1.28.0.tar.gz
+wget http://nginx.org/download/nginx-1.26.2.tar.gz
+tar xvf nginx-1.26.2.tar.gz
 git clone https://github.com/SpiderLabs/ModSecurity-nginx.git
 git clone https://github.com/jaweewo/nginx-ntlm-module.git
 
-cd nginx-1.28.0
+cd nginx-1.26.2
 ./configure --prefix=/usr/local/nginx \
   --with-http_ssl_module \
-  --with-http_stub_status_module \
   --add-module=../ModSecurity-nginx \
   --add-module=../nginx-ntlm-module \
   --with-cc-opt="-Wno-error"
 make && make install
 cd ..
-rm -rf nginx-1.28.0 nginx-1.28.0.tar.gz ModSecurity-nginx nginx-ntlm-module
-
+rm -rf nginx-1.26.2 nginx-1.26.2.tar.gz ModSecurity-nginx nginx-ntlm-module
 
 echo "Setting up ModSecurity configuration..."
 mkdir -p /usr/local/nginx/conf/modsecurity
