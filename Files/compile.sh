4a50a3e3f01d538a22f18481b308fd02ec01c9a0
Javier Perez <javierosp97@gmail.com>
2026-06-03 14:57:20 +0200

    Fix async

--- Files/compile.sh
+++ Files/compile.sh
@@ -88,7 +88,7 @@ echo "Downloading and installing OpenSSL..."
 wget https://www.openssl.org/source/openssl-1.1.1.tar.gz
 tar xvf openssl-1.1.1.tar.gz
 cd openssl-1.1.1
-./config --prefix=/usr --openssldir=/etc/ssl --libdir=lib no-shared zlib-dynamic
+./config --prefix=/usr --openssldir=/etc/ssl --libdir=lib no-shared zlib-dynamic no-async
 make && make install
 cd ..
 rm -rf openssl-1.1.1.tar.gz openssl-1.1.1
